adb0eff383a001872df099ef340486976aaef045
Tomas <capucchiotomas@gmail.com>
2023-11-03 18:11:33 -0300

    Agrego mi modelo de deep learning

--- Código.R
+++ Código.R
@@ -11,6 +11,9 @@ bank_train <- read.delim("bank_train.txt")
 View(bank_test)
 View(bank_train)
 
+
+"Deep Learning"
+
 set.seed(2023)
 
 #Separamos en datos para entrenar el modelo y en datos para validarlo
@@ -18,7 +21,29 @@ filas <- createDataPartition(bank_train$y,p = 0.80 )[[1]]
 train <- slice(bank_train, filas)
 validate <- slice(bank_train, -filas)
 
+grilla <- expand.grid(size = c(3, 5, 8), decay = c(0.1, 1.5, 5))
+
+dl <- train(y ~ ., 
+            data = train,
+            method = "nnet",
+            tuneGrid = grilla,
+            trControl = trainControl(method = "cv", number = 5), 
+            maxit = 90,
+            trace = FALSE)
+
+dl
+
+confusionMatrix(
+  as.factor(validate$y), 
+  predict(dl, newdata = validate)
+)
+
+predicciones_test_dl <- predict(dl, newdata = bank_test)
+
+bank_test$predicciones <- prediccionesdl
+
 "Arboles de decision"
+
 set.seed(2013)
 
 ajuste <- train(
