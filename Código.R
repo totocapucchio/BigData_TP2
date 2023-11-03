1ce30d1e45dfe8f2e70655d5f742c8b4b23f3f9c
Tomas <capucchiotomas@gmail.com>
2023-11-03 17:34:07 -0300

    commit cambios tomas inicio modelo deep

--- Código.R
+++ Código.R
@@ -10,7 +10,12 @@ bank_train <- read.delim("bank_train.txt")
 View(bank_test)
 View(bank_train)
 
+set.seed(2023)
+
 #Separamos en datos para entrenar el modelo y en datos para validarlo
 filas <- createDataPartition(bank_train$y,p = 0.80 )[[1]]
 train <- slice(bank_train, filas)
 validate <- slice(bank_train, -filas)
+
+predict()
+print(bank_test)
