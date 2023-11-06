9824eb203099b3b981dbfb98c7e31ecb071e32b9
Tomas <capucchiotomas@gmail.com>
2023-11-05 23:31:53 -0300

    Agrego todos los cambios de deep learning y el nuevo archivo con solo las predicciones de test

--- Código.R
+++ Código.R
@@ -1,4 +1,3 @@
-library(usethis)
 library(tidyverse)
 library(lattice)
 library(ggplot2)
@@ -8,7 +7,7 @@ library(NeuralNetTools)
 
 bank_test <- read.delim("bank_test.txt")
 bank_train <- read.delim("bank_train.txt")
-View(bank_test)
+  View(bank_test)
 View(bank_train)
 
 
@@ -21,26 +20,45 @@ filas <- createDataPartition(bank_train$y,p = 0.80 )[[1]]
 train <- slice(bank_train, filas)
 validate <- slice(bank_train, -filas)
 
-grilla <- expand.grid(size = c(3, 5, 8), decay = c(0.1, 1.5, 5))
+grilla <- expand.grid(size = c(3, 5, 8), decay = c(3, 1.5, 5))
 
 dl <- train(y ~ ., 
             data = train,
             method = "nnet",
             tuneGrid = grilla,
             trControl = trainControl(method = "cv", number = 5), 
-            maxit = 90,
+            maxit = 200,
             trace = FALSE)
 
 dl
 
 confusionMatrix(
-  as.factor(validate$y), 
-  predict(dl, newdata = validate)
+  as.factor(train$y), 
+  predicciones_train <- predict(dl, newdata = train, type = "raw")
 )
 
-predicciones_test_dl <- predict(dl, newdata = bank_test)
+#Predecimos los valores para los datos de validación y vemos su desempeño
 
-bank_test$predicciones <- prediccionesdl
+confusionMatrix(
+  as.factor(validate$y), 
+  predicciones_validate <- predict(dl, newdata = validate, type = "raw")
+)
+
+# Generamos las predicciones para test
+
+predicciones_test <- predict(dl, newdata = bank_test)
+
+bank_test$y <- predicciones_test
+
+view(bank_test)
+
+# Conservar solo la columna "y"
+
+grupo4 <- bank_test["y"]
+
+#Exportar los datos con solo la columna y
+
+write.table(grupo4, "grupo4.txt", sep = "\t", row.names = FALSE)
 
 "Arboles de decision"
 
@@ -65,6 +83,8 @@ predicciones_rf_validate <- predict(ajuste, newdata = validate)
 
 validate$predicciones <- predicciones_rf_validate
 
+"KNN"
+
 # Vectores numéricos para guardar las medidas de error (una para cada k candidato)
 train_error <- NULL
 test_error <- NULL
@@ -74,8 +94,6 @@ candidatos <- c(5, 10, 25, 40, 60, 70)
 
 # Con una estructura iterativa repetimos el proceso para cada valor de k a probar
 for (k in candidatos) {
-  
-  
   
   algoritmo <- train(
     y ~ .,
